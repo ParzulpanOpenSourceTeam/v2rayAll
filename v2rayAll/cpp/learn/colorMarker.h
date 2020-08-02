33824b9b5bcdf53c8d60d4a863fc6a1de78702a1
parzulpan <parzulpan@gmail.com>
2020-08-02 23:45:28 +0800

    增加: QML和C++混合编程;

--- v2rayAll/cpp/learn/colorMarker.h
+++ v2rayAll/cpp/learn/colorMarker.h
@@ -1,1 +1,32 @@
-#include <algorithm>
+#include <QObject>
+#include <QColor>
+#include <QString>
+#include <string>
+
+class ColorMaker: public QObject
+{
+    Q_OBJECT
+    Q_ENUMS(GenerateAlgorithm)
+public:
+    ColorMaker(QObject *parent() = 0);
+    ~ColorMaker();
+
+    enum GenerateAlgorithm {
+        RandomRGB,
+        RandomRed,
+        RandomGreen,
+        RandomBlue,
+        LinearIncrease
+    };
+
+    Q_INVOKABLE GenerateAlgorithm algorithm() const;
+    Q_INVOKABLE void setAlgorithm(GenerateAlgorithm &algorithm);    // 颜色生成算法
+
+signals:
+    void colorChanged(const QColor &color);
+    void currentTime(const QString &strTime);
+
+public slots:
+    void start();
+    void stop();
+};
